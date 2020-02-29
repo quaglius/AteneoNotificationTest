97943e165a3b026329cd013cac92f4effe2fc18a
Quaglius <daniel.quagliano@gmail.com>
2020-02-29 20:59:10 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ateneonotificationtest/NotificationSettings.java
@@ -0,0 +1,6 @@
+package com.example.ateneonotificationtest;
+
+public class NotificationSettings {
+    public static String HubName = "ateneohub";
+    public static String HubListenConnectionString = "Endpoint=sb://ateneo.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=7Gh0Nr7CiFVj2tdHE381k7Cl3Zfsw+NAZXLR1UnW748=";
+}
